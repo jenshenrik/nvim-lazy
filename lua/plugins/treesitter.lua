a75077b76fab8cc9b3d540be43e2bbf68cd781fa
Jens Henrik Vogelius <jens.vogelius@lego.com>
2025-09-29 08:22:12 +0200

    rust

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
-  return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "hyprlang",
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "hyprlang",
+      "vim",
+      "lua",
+      "vimdoc",
+      "html",
+      "css",
 
-        "c_sharp",
-      },
+      "c_sharp",
+      "rust",
     },
-  }
+  },
+}
